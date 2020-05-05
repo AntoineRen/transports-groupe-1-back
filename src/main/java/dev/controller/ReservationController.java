db6e3503ad60bf296dbd357f4121e9b76211f68b
Antoine Renou <antoine.renou.pro@gmail.com>
2020-05-05 22:46:38 +0200

    AR-Creation-exception-requete-reservation-email : Save 05/05 #7

--- src/main/java/dev/controller/ReservationController.java
+++ src/main/java/dev/controller/ReservationController.java
@@ -4,14 +4,19 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.entites.Reservation;
 import dev.entites.dto.ReservationDto;
+import dev.exceptions.CollegueNonTrouveException;
 import dev.service.ReservationService;
 
 @RestController
@@ -39,6 +44,40 @@ public class ReservationController {
 	public Reservation postReservation(@RequestBody @Valid ReservationDto reservationDto) {
 
 		return this.reservationService.postReservation(reservationDto);
+	}
+
+	/**
+	 * Réceptionne une requete sur l'url back_url/reservation/?email="..." et
+	 * renvoie la liste des réservations en cours de véhicule de société du collègue
+	 * possésant cet email
+	 * 
+	 * @param email
+	 * @return Reservation
+	 */
+	@GetMapping("current")
+	public List<Reservation> getReservationByEmailEnCours(@RequestParam String email) {
+
+		return this.reservationService.getReservationByEmailEnCours(email);
+	}
+
+	/**
+	 * Réceptionne une requete sur l'url back_url/reservation/?email="..." et
+	 * renvoie la liste des réservations passées, de véhicule de société du collègue
+	 * possésant cet email
+	 * 
+	 * @param email
+	 * @return Reservation
+	 */
+	@GetMapping("histo")
+	public List<Reservation> getReservationByEmailHisto(@RequestParam String email) {
+
+		return this.reservationService.getReservationByEmailHisto(email);
+	}
+
+	@ExceptionHandler(CollegueNonTrouveException.class)
+	public ResponseEntity<String> onCollegueNonTrouveException(CollegueNonTrouveException e) {
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 	}
 
 }
--- src/main/java/dev/repository/CollegueRepository.java
+++ src/main/java/dev/repository/CollegueRepository.java
@@ -1,5 +1,7 @@
 package dev.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import dev.entites.Collegue;
 
 @Repository
 public interface CollegueRepository extends JpaRepository<Collegue, Long> {
+
+	Optional<Collegue> findOneByEmail(String email);
 
 }
