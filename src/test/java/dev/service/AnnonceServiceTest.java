b148d316e598b0589a54076ef50bbd96229955f5
Jules Dupuis <jules.dupuis51@gmail.com>
2020-05-09 16:46:57 +0200

    JD- ListReservationCovoiturage: Correction Test Controller #1

--- src/test/java/dev/service/AnnonceServiceTest.java
+++ src/test/java/dev/service/AnnonceServiceTest.java
@@ -81,9 +81,9 @@ class AnnonceServiceTest {
 
 		Itineraire itinerairePast = new Itineraire(LocalDateTime.now().minusDays(5), LocalDateTime.now().minusDays(1),
 				"test", "test", 100, 100D);
-		Itineraire itineraireProxPast = new Itineraire(LocalDateTime.now().minusMinutes(5),
+		Itineraire itineraireProxPast = new Itineraire(LocalDateTime.now().minusMinutes(4),
 				LocalDateTime.now().minusMinutes(5), "test", "test", 100, 100D);
-		Itineraire itineraireProxFuture = new Itineraire(LocalDateTime.now().plusMinutes(5),
+		Itineraire itineraireProxFuture = new Itineraire(LocalDateTime.now().plusMinutes(6),
 				LocalDateTime.now().plusMinutes(5), "test", "test", 100, 100D);
 		Itineraire itineraireFuture = new Itineraire(LocalDateTime.now().plusDays(5), LocalDateTime.now().plusDays(5),
 				"test", "test", 100, 100D);
@@ -141,7 +141,7 @@ class AnnonceServiceTest {
 	@Test
 	void testGetAnnonceEnCours() {
 
-		assertEquals(1, this.annonceService.getAnnonceEnCours(listAnnoncesTime).size());
+		assertEquals(2, this.annonceService.getAnnonceEnCours(listAnnoncesTime).size());
 		assertThat(this.annonceService.getAnnonceEnCours(listAnnoncesTime).contains(annoncePast));
 
 	}
