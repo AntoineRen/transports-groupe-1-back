bc8dc46010d4e5962c76d55be63bf05fa378dc8e
Jules Dupuis <jules.dupuis51@gmail.com>
2020-05-17 16:47:05 +0200

    JD-DetailsVehiclue issus 11

--- src/main/java/dev/repository/VehiculeSocieteRepository.java
+++ src/main/java/dev/repository/VehiculeSocieteRepository.java
@@ -1,11 +1,17 @@
 package dev.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import dev.entites.Reservation;
 import dev.entites.VehiculeSociete;
 
 @Repository
 public interface VehiculeSocieteRepository extends JpaRepository<VehiculeSociete, Long> {
 
+
+	VehiculeSociete findOneByImmatriculation(String immatriculation);
+	
 }
