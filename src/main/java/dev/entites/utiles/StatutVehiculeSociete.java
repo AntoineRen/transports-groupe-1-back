9ee29c21a53c005ce976dfde07caf4f2fd535aae
Antoine Renou <antoine.renou.pro@gmail.com>
2020-05-18 09:03:06 +0200

    la ligne

--- src/main/java/dev/entites/utiles/StatutVehiculeSociete.java
+++ src/main/java/dev/entites/utiles/StatutVehiculeSociete.java
@@ -2,25 +2,25 @@ package dev.entites.utiles;
 
 public enum StatutVehiculeSociete {
 	EN_SERVICE("En service"), EN_REPARATION("En réparation"), HORS_SERVICE("Hors service");
-	
+
 	private String detail;
-	
+
 	private StatutVehiculeSociete(String detail) {
 		this.detail = detail;
 	}
-	
+
 	public String getDetail() {
 		return detail;
 	};
-	
+
 	public static StatutVehiculeSociete getName(String nom) {
 
-        for(StatutVehiculeSociete c : StatutVehiculeSociete.values()) {
+		for (StatutVehiculeSociete c : StatutVehiculeSociete.values()) {
 
-            if(c.getDetail().equals(nom)) {
-                return c;
-            }
-        }
-        return null; //TODO throw une exception
-    }
+			if (c.getDetail().equals(nom)) {
+				return c;
+			}
+		}
+		return null; // TODO throw une exception
+	}
 }
