01302f36aa541012eb50ddae8ea552313cd8a0e0
Jules Dupuis <jules.dupuis51@gmail.com>
2020-05-18 15:18:38 +0200

    JD-CycleVehicule : ajout controller et service pourstatut vehicule

--- src/main/java/dev/controller/VehiculeController.java
+++ src/main/java/dev/controller/VehiculeController.java
@@ -4,17 +4,24 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.entites.Reservation;
 import dev.entites.VehiculeSociete;
 import dev.entites.dto.DisponibiliteDto;
 import dev.entites.dto.PeriodeDto;
 import dev.entites.dto.VehiculeSocieteDto;
+import dev.exceptions.ApplicationException;
 import dev.service.VehiculeSocieteService;
 
 @RestController
@@ -62,5 +69,30 @@ public class VehiculeController {
 	public List<DisponibiliteDto> disponibilitesVehicules(@RequestBody @Valid PeriodeDto periodeDto) {
 
 		return this.vehiculeService.disponibilitesVehicules(periodeDto);
+	}
+	
+	
+	/**
+	 * Permet le changement de statut du vehicule : EN_REPARATION, EN_SERVICE, HORS_SERVICE
+	 * @param statut
+	 * @param immat
+	 * @return
+	 * @throws Exception
+	 */
+	@PutMapping(params = {"statut","immat"})
+	public VehiculeSociete putStatusVehicule(@RequestParam String statut,@RequestParam String immat){
+		return this.vehiculeService.putStatutVehiculeService(statut, immat);
+	}
+	/**
+	 * Catch les exceptions throw par le service et renvoie une responseEntity avec
+	 * le statut not found et le message d'erreur
+	 * 
+	 * @param e
+	 * @return ResponseEntity<String>
+	 */
+	@ExceptionHandler(ApplicationException.class)
+	public ResponseEntity<String> onApplicationException(ApplicationException e) {
+
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 	}
 }
--- src/main/java/dev/repository/VehiculeSocieteRepository.java
+++ src/main/java/dev/repository/VehiculeSocieteRepository.java
@@ -1,6 +1,7 @@
 package dev.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,6 @@ import dev.entites.VehiculeSociete;
 public interface VehiculeSocieteRepository extends JpaRepository<VehiculeSociete, Long> {
 
 
-	VehiculeSociete findOneByImmatriculation(String immatriculation);
+	Optional<VehiculeSociete> findOneByImmatriculation(String immatriculation);
 	
 }
